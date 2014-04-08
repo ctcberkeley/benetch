467f71d11983e7b731bbd5c1ab4236dd45d417fa
Tim Hyon <tim.c.hyon@gmail.com>
2014-04-07 17:43:42 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Benetech::Application.config.secret_key_base = '3be3c07a930e18d5d17d3e5091feebae40934bc6f22559596408bb1ad032d4e3cd5f626d2aa2d3bc12198cfaa490846b0371ea631bb8f3c4ff2a1c371bc01747'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Benetech::Application.config.session_store :cookie_store, key: '_benetech_session'
