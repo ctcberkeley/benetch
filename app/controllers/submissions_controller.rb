85cdd2e306e0df34ae6121fe81016d34ec45d203
amtrak24 <jacobsjmd01@comcast.net>
2014-04-14 19:58:37 -0700

    Creates submissions and welcome controllers

---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,7 @@
+class SubmissionsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+  def submissions_create
+  end
+end
